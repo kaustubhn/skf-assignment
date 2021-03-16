03a52c7001937f80b75a27be0a0a8850751777b9
kaustubhn <kasutubhnarkhede88@gmail.com>
2021-03-16 16:23:03 +0530

    Add basic api for publish and get

--- app.py
+++ app.py
@@ -1,23 +1,62 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask import make_response, abort
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_USE_SSL, REDIS_SSL_CERT
+from config import FLASK_HOST, FLASK_PORT, FLASK_DEBUG
 import redis
 
-redis_host = "localhost"
-redis_port = 6379
-redis_password = ""
+try:
+    print('Trying to connect to Redis')
+    r = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB
+    )
+    print('Successfully connected to Redis server')
+except Exception as e:
+    print("Error: Unable to connect to redis.")
+    print(e)
+    
 
 app = Flask(__name__)
 
-@app.route('/publish', methods=['POST'])
+
+# Error handling
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def invalid_body(error):
+    return make_response(jsonify({'error': 'Invalid request body'}), 400)
+
+
+# Routes
+@app.route('/api/v1/publish', methods=['POST'])
 def publish():
-    return 'Hello, World!'
+    if request.method == 'POST':
+        if not request.json or not 'content' in request.json:
+            abort(400)
+        else:
+            try:
+                r.set("content",request.json["content"])
+                return make_response(jsonify({"status": "success"}), 200)
+            except Exception as e:
+                print(e)
+                return make_response(jsonify({"status": "error"}), 500)
+    else:
+        return jsonify({"error": "Invalid Method"})
 
-@app.route('/getLast', methods=['GET'])
+@app.route('/api/v1/getLast', methods=['GET'])
 def get_last():
-    return 'Hello, World!'
+    if request.method == 'GET':
+        return make_response(jsonify({"content": r.get('content')}, 200))
+    else:
+        return jsonify({"error": "Invalid Method"})
 
-@app.route('/getByTime', methods=['GET'])
+@app.route('/api/v1/getByTime', methods=['GET'])
 def get_by_time():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0")
+   app.run(host=FLASK_HOST, port=FLASK_PORT, debug=FLASK_DEBUG)
+   
---
+++ config.py
@@ -0,0 +1,14 @@
+# Config file will have configurations related to the REDIS and other services used in the application
+
+
+# Flask config
+FLASK_HOST = '0.0.0.0'
+FLASK_PORT = 5000
+FLASK_DEBUG = True
+
+# Redis config
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+REDIS_DB = 0
+REDIS_USE_SSL = False
+REDIS_SSL_CERT = None
